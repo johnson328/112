96d4b1c5b3d25f10724fc07ed8b97f7fd4c98c77
johnson328 <131642177+johnson328@users.noreply.github.com>
2023-06-05 14:02:37 +0800

    Update script.js

--- 112/dist/script.js
+++ 112/dist/script.js
@@ -1,1 +1,7 @@
-document.write("HELLO我是李聖柏 ")
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
